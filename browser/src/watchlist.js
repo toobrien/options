3c95514c34d0375db73b15c5990b599c6867141b
Taylor O'Brien <toobrien@amazon.com>
2021-03-04 11:30:23 -0800

    fixed scope issue in watchlist

--- browser/src/watchlist.js
+++ browser/src/watchlist.js
@@ -1,74 +1,5 @@
 
 class watchlist {
-    chart_width = 300;
-    chart_height = 200;
-    row_width = 5;
-    prefix = "https://www.quandl.com/data/CHRIS/";
-    list = {
-        "equity index": {
-            "s&p 500": "CME_ES1",
-            "nasdaq 100": "CME_NQ1",
-            "dow jones": "CME_YM1",
-            "russel 2000": "ICE_TF1",
-            "nikkei": "OSE_NK2251",
-            "dax": "EUREX_FDAX1",
-        },
-        "bonds": {
-            "10 year note": "CME_TY1",
-            "treasury bond": "CME_US1",
-            "bund": "EUREX_FGBL1",
-            "bobl": "EUREX_FGBM1",
-            "long gilt": "LIFFE_R1"
-        },
-        "currencies": {
-            "british pound": "CME_BP1",
-            "mexican peso": "CME_MP1",
-            "japanese yen": "CME_JY1",
-            "swiss franc": "CME_SF1",
-            "canadian dollar": "CME_CD1",
-            "australian dollar": "CHRIS/CME_AD1",
-            "euro": "CHRIS/CME_E71",
-            "russian ruble": "CHRIS/CME_RU1",
-            "brazilian real": "CHRIS/CME_BR1"
-        },
-        "volatility": {
-            "s&p (vix)": "CHRIS/CBOE_VX1",
-            "nasdaq": "CHRIS/CBOE_VN1",
-            "vstoxx": "CHRIS/EUREX_FVS1",
-            "gold": "CHRIS/CBOE_GV1",
-            "oil": "CHRIS/CBOE_OV1"
-        },
-        "softs": {
-            "cocoa": "CHRIS/LIFFE_C1",
-            "orange juice": "CHRIS/ICE_OJ1",
-            "sugar #11": "CHRIS/ICE_SB1",
-            "coffee": "CHRIS/ICE_KC5",
-            "cotton #2": "CHRIS/ICE_CT1",
-            "lumber": "CHRIS/CME_LB1"
-        },
-        "agricultural": {
-            "wheat": "CHRIS/CME_W1",
-            "corn": "CHRIS/CME_C1",
-            "soybeans": "CHRIS/CME_S1",
-            "soybean meal": "CHRIS/CME_SM1",
-            "soybean oil": "CHRIS/CME_BO1",
-            "lean hogs": "CHRIS/CME_LN1",
-            "feeder cattle": "CHRIS/CME_FC1"
-        },
-        "metals": {
-            "copper": "CHRIS/CME_HG1",
-            "platinum": "CHRIS/CME_PL1",
-            "gold": "CHRIS/CME_GC1",
-            "silver": "CHRIS/CME_SI1",
-            "palladium": "CHRIS/CME_PA1"
-        },
-        "energy": {
-            "natural gas": "CHRIS/CME_NG1",
-            "crude oil": "CHRIS/CME_CL1",
-            "gas oil": "CHRIS/ICE_G1",
-            "heating oil": "CHRIS/ICE_O1"
-        }
-    };
 
     build_table() {
         let table = document.createElement("table");
@@ -157,6 +88,75 @@ class watchlist {
     }
 
     constructor() {
+        this.chart_width = 300;
+        this.chart_height = 200;
+        this.row_width = 5;
+        this.prefix = "https://www.quandl.com/data/CHRIS/";
+        this.list = {
+            "equity index": {
+                "s&p 500": "CME_ES1",
+                "nasdaq 100": "CME_NQ1",
+                "dow jones": "CME_YM1",
+                "russel 2000": "ICE_TF1",
+                "nikkei": "OSE_NK2251",
+                "dax": "EUREX_FDAX1",
+            },
+            "bonds": {
+                "10 year note": "CME_TY1",
+                "treasury bond": "CME_US1",
+                "bund": "EUREX_FGBL1",
+                "bobl": "EUREX_FGBM1",
+                "long gilt": "LIFFE_R1"
+            },
+            "currencies": {
+                "british pound": "CME_BP1",
+                "mexican peso": "CME_MP1",
+                "japanese yen": "CME_JY1",
+                "swiss franc": "CME_SF1",
+                "canadian dollar": "CME_CD1",
+                "australian dollar": "CHRIS/CME_AD1",
+                "euro": "CHRIS/CME_E71",
+                "russian ruble": "CHRIS/CME_RU1",
+                "brazilian real": "CHRIS/CME_BR1"
+            },
+            "volatility": {
+                "s&p (vix)": "CHRIS/CBOE_VX1",
+                "nasdaq": "CHRIS/CBOE_VN1",
+                "vstoxx": "CHRIS/EUREX_FVS1",
+                "gold": "CHRIS/CBOE_GV1",
+                "oil": "CHRIS/CBOE_OV1"
+            },
+            "softs": {
+                "cocoa": "CHRIS/LIFFE_C1",
+                "orange juice": "CHRIS/ICE_OJ1",
+                "sugar #11": "CHRIS/ICE_SB1",
+                "coffee": "CHRIS/ICE_KC5",
+                "cotton #2": "CHRIS/ICE_CT1",
+                "lumber": "CHRIS/CME_LB1"
+            },
+            "agricultural": {
+                "wheat": "CHRIS/CME_W1",
+                "corn": "CHRIS/CME_C1",
+                "soybeans": "CHRIS/CME_S1",
+                "soybean meal": "CHRIS/CME_SM1",
+                "soybean oil": "CHRIS/CME_BO1",
+                "lean hogs": "CHRIS/CME_LN1",
+                "feeder cattle": "CHRIS/CME_FC1"
+            },
+            "metals": {
+                "copper": "CHRIS/CME_HG1",
+                "platinum": "CHRIS/CME_PL1",
+                "gold": "CHRIS/CME_GC1",
+                "silver": "CHRIS/CME_SI1",
+                "palladium": "CHRIS/CME_PA1"
+            },
+            "energy": {
+                "natural gas": "CHRIS/CME_NG1",
+                "crude oil": "CHRIS/CME_CL1",
+                "gas oil": "CHRIS/ICE_G1",
+                "heating oil": "CHRIS/ICE_O1"
+            }
+        };
     }
 }
 
